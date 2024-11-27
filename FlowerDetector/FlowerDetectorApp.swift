512c5a5388c9b85090ee655900769132962c7364
Ibrahim Isaac <ibrahimisaacmy@icloud.com>
2024-11-27 12:39:37 +0800

    Initial Commit

---
+++ FlowerDetector/FlowerDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowerDetectorApp.swift
+//  FlowerDetector
+//
+//  Created by STDC_13 on 27/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct FlowerDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlowerDetectorTests/FlowerDetectorTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlowerDetectorTests.swift
+//  FlowerDetectorTests
+//
+//  Created by STDC_13 on 27/11/2024.
+//
+
+import Testing
+@testable import FlowerDetector
+
+struct FlowerDetectorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
